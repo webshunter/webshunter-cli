d0f7b55106026bddccd11e9af1d39fe4535d8895
webshunter <webshunternet@gmail.com>
2024-01-27 20:18:57 +0700

    first commit

---
+++ cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const [,,...args] = process.argv;
+const fs = require('fs');
+const path = require('path');
+const DIR = __dirname;
+const { render, renderFile } = require('template-file');
+const {package} = require('./command/package');
+const {consoleColor} = require('./command/color');
+
+let [command, arg1, arg2, ...arg3] = args
+
+if(command){
+    command === 'package' ? (function(){
+        consoleColor(`\n`);
+        consoleColor(`WEBSHUNTER PACKAGE LIST\n`, 'yellow');
+        consoleColor(`berikut merupakan daftar package yang tersedia di webshunter mulai dari npm dan composer.\n`);
+        package.map(function (r, i) {
+            return Object.keys(r).map(function (s) {
+                consoleColor(' || '+s,'green',null, r[s] );
+            })
+        });
+        consoleColor(`\n`);
+        return true;
+    })() 
+    : null;
+}else{
+    consoleColor(`\n`);
+    consoleColor(`WEBSHUNTER`, 'yellow');
+    consoleColor(` berikut command yang tersedia di webshunter :`);
+    consoleColor(` 1) package`,'green',null,'berisi list package yang tersedia');
+    consoleColor(`\n`);
+}
---
+++ command/package.js
@@ -0,0 +1,7 @@
+module.exports.package = [
+    {
+        'npm': 'npm i webshunter-dom-element',
+        'github': 'https://github.com/webshunter/webshunter-dom-element.git',
+        'fungsi': 'berguna untuk membuat document object model'
+    }
+];
